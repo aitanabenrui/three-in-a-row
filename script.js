907e541b300e260cda91173e8d292d7a0a4f04d7
Aitana <aitanabenrui2001@gmail.com>
2025-02-04 13:21:25 +0100

    feat: add function addPiece

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const start = document.querySelector("#start");
 const startForm = document.querySelector("#start-form");
 const players = document.querySelector(".players");
 const grid = document.querySelector("#grid");
-
-
+let numberOfPlays = 0; //contador cada vez que se hace click en un botón, si es par en el grid se añadirá una cosa, si es impar otra
 //Aquí añadiremos las funciones necesarias para que el juego funcione
 
 const startGame = (e) =>{
@@ -15,6 +14,23 @@ const startGame = (e) =>{
     players.classList.remove('inv');
     grid.classList.remove('inv');
 }
+
+const addPiece =()=>{
+    if(numberOfPlays === 0 || numberOfPlays % 2 === 0 ){
+        document.querySelector(".cell").innerHTML = "⭕";
+    } else  {
+        document.querySelector(".cell").innerHTML = "❌";
+    }
+
+    numberOfPlays += 1;
+    winnerOrLooser();
+}; 
+
+const winnerOrLooser = () =>{
+    //comprobar si ha hecho 3 en raya o no, cada vez que se añadee una ficha se debe comprobar que los botones no contengan carácteres iguales alineados de cualquier forma
+
+}
+
 
 
 
@@ -29,3 +45,9 @@ const reset = ()=>{
 //añadiremos aquí los addEventListeners, llaman a las funciones necesarias al interactuar con el dom
 
 start.addEventListener('click', startGame); //cuando clickamos en start, empezará la aprtida
+
+grid.addEventListener('click', function(event){
+    if(event.target.classList.contains("cell")){
+        addPiece();
+    }
+})
