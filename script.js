98aa52dacf92f6f3a431b70ca03097fe257cb2ce
Aitana <aitanabenrui2001@gmail.com>
2025-02-04 12:17:14 +0100

    feat: first code lines three-in-a-row

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+//Aquí irán la definición de las variables desde las que podremos acceder al html: a elementos del DOM
+//para ello podremos usar querySelector(selector) o getElementById / getElementsByClassName / getElementsByTagName
+
+const start = document.querySelector("#start");
+const startForm = document.querySelector("#start-form");
+const players = document.querySelector(".players");
+const grid = document.querySelector("#grid");
 
 
+//Aquí añadiremos las funciones necesarias para que el juego funcione
 
+const startGame = (e) =>{
+    e && e.preventDefault();   //todos los formularios recargan la página, con esto lo evitamos, por si a caso aún que no haya un form
+    startForm.classList.add('inv'); //al empezar la partida añadimos a start la clase inv para que no se muestre
+    players.classList.remove('inv');
+    grid.classList.remove('inv');
+}
 
 
 
@@ -12,4 +26,6 @@ const reset = ()=>{
     location.reload();
 };
 
-//añadiremos aquí los addEventListeners
+//añadiremos aquí los addEventListeners, llaman a las funciones necesarias al interactuar con el dom
+
+start.addEventListener('click', startGame); //cuando clickamos en start, empezará la aprtida
